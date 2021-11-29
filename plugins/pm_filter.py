cda750a3097249ca4d411308f2dffec26cf82bdc
mallumovieleech <88672268+mallumovieleech@users.noreply.github.com>
2021-11-30 01:02:03 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -724,6 +724,6 @@ async def advantage_spell_chok(msg):
                 )
             ] for k, movie in enumerate(movielist)]
     btn.append([InlineKeyboardButton(text="Close", callback_data=f'spolling#{user}#close_spellcheck')])
-    await msg.reply("I couldn't find anything related to that\nDid you mean any one of these👇?\nClick on Movie name if your movie name on below list.👇\n<b>നിങ്ങൾ ഉദ്ദേശിച്ച മൂവി താഴെ കാണുന്നതിൽ ഉണ്ടോ?👇.</b>\n<b>ഉണ്ടെങ്കിൽ അതിൽ ക്ലിക്ക് ചെയ്യുക.</b>\n<b>ഇല്ലങ്കിൽ സ്പെല്ലിംഗ് ഗൂഗിളിൽ നോക്കി ഒന്നോടെ ടൈപ്പ് ചെയ്യുക</b>👇", reply_markup=InlineKeyboardMarkup(btn))
+    await msg.reply("I couldn't find anything related to that\nDid you mean any one of these👇?\nClick on Movie name if your movie name on below list.👇\n<b>നിങ്ങൾ ഉദ്ദേശിച്ച മൂവി താഴെ കാണുന്നതിൽ ഉണ്ടോ?👇.</b>\n<b>ഉണ്ടെങ്കിൽ അതിൽ ക്ലിക്ക് ചെയ്യുക.</b>\n<b>ഇല്ലങ്കിൽ സ്പെല്ലിംഗ് ഗൂഗിളിൽ നോക്കി ഒന്നോടെ വർഷവും കൂട്ടി ടൈപ്പ് ചെയ്യുക</b>👇", reply_markup=InlineKeyboardMarkup(btn))
     
 
